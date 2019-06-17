17d13b85f86453473071023c5ca2c53dcb6e59aa
rkapl123 <rolandk@kapl.org>
2019-06-18 01:01:33 +0200

    AboutBox1 as well

---
+++ source/AboutBox1.vb
@@ -0,0 +1,31 @@
+﻿Public NotInheritable Class AboutBox1
+    Private Sub AboutBox1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        ' Set the title of the form.
+        Dim sModuleInfo As String = vbNullString
+        For Each tModule As ProcessModule In Process.GetCurrentProcess().Modules
+            Dim sModule As String = tModule.FileName
+            If sModule.ToUpper.Contains("DBADDIN-ADDIN-PACKED.XLL") Or sModule.ToUpper.Contains("DBADDIN-ADDIN64-PACKED.XLL") Then
+                sModuleInfo = FileDateTime(sModule).ToString()
+            End If
+        Next
+
+        Me.Text = String.Format("About {0}", My.Application.Info.Title)
+        Me.LabelProductName.Text = My.Application.Info.ProductName
+        Me.LabelVersion.Text = String.Format("Version {0} Buildtime {1}", My.Application.Info.Version.ToString, sModuleInfo)
+        Me.LabelCopyright.Text = My.Application.Info.Copyright
+        Me.LabelCompanyName.Text = "Help and Sources on: " + My.Application.Info.CompanyName
+        Me.TextBoxDescription.Text = My.Application.Info.Description
+    End Sub
+
+    Private Sub OKButton_Click(ByVal sender As Object, ByVal e As EventArgs) Handles OKButton.Click
+        Dim errStr As String = vbNullString
+        initSettings()
+        createConfigTreeMenu(True)
+        MsgBox("refreshed ConfigTreeMenu and restarted theDBSheetAppHandler", vbInformation + vbOKOnly, "DBAddin: refresh Config tree...")
+        Me.Close()
+    End Sub
+
+    Private Sub LabelCompanyName_Click(sender As Object, e As EventArgs) Handles LabelCompanyName.Click
+        Process.Start(My.Application.Info.CompanyName)
+    End Sub
+End Class
