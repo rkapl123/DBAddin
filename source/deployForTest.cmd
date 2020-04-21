737c001a89a50fa7819a6a3c29ebf5c59deec65e
rkapl123 <rolandk@kapl.org>
2020-04-21 21:53:22 +0200

    bump version

--- source/deployForTest.cmd
+++ source/deployForTest.cmd
@@ -3,11 +3,6 @@ Set /P answr=deploy (r)elease (empty for debug)?
 set source=bin\Debug
 If "%answr%"=="r" (
 	set source=bin\Release
-	copy /Y %source%\DBaddin-AddIn64-packed.xll "..\Distribution\DBaddin64.xll"
-	copy /Y %source%\DBaddin-AddIn-packed.xll "..\Distribution\DBaddin32.xll"
-	copy /Y %source%\DBaddin.dll.config "..\Distribution\DBaddin.xll.config"
-	copy /Y DBaddinCentral.config "..\Distribution\DBaddinCentral.config"
-	copy /Y DBAddinUser.config "..\Distribution\DBaddinUser.config"
 )
 if exist "C:\Program Files\Microsoft Office\root\" (
 	echo 64bit office
@@ -23,5 +18,13 @@ if exist "C:\Program Files\Microsoft Office\root\" (
 	copy /Y %source%\DBaddin.dll.config "%appdata%\Microsoft\AddIns\DBaddin.xll.config"
 	copy /Y DBaddinCentral.config "%appdata%\Microsoft\AddIns\DBaddinCentral.config"
 	copy /Y DBAddinUser.config "%appdata%\Microsoft\AddIns\DBaddinUser.config"
+)
+if "%source%"=="bin\Release" (
+	echo copying Distribution
+	copy /Y %source%\DBaddin-AddIn64-packed.xll "..\Distribution\DBaddin64.xll"
+	copy /Y %source%\DBaddin-AddIn-packed.xll "..\Distribution\DBaddin32.xll"
+	copy /Y %source%\DBaddin.dll.config "..\Distribution\DBaddin.xll.config"
+	copy /Y DBaddinCentral.config "..\Distribution\DBaddinCentral.config"
+	copy /Y DBAddinUser.config "..\Distribution\DBaddinUser.config"
 )
 pause
