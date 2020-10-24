b2cb6f41c8acf3aec8a1fc85c1bac43c0b09af2f
rkapl123 <rolandk@kapl.org>
2020-10-24 21:28:53 +0200

    bump version

--- source/My Project/AssemblyInfo.vb
+++ source/My Project/AssemblyInfo.vb
@@ -31,6 +31,6 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("1.0.0.6")>
-<Assembly: AssemblyFileVersion("1.0.0.6")>
+<Assembly: AssemblyVersion("1.0.0.7")>
+<Assembly: AssemblyFileVersion("1.0.0.7")>
 <Assembly: NeutralResourcesLanguage("de-DE")>
